e87447bb5f34dd3ef9d3dd1c3e0eab141e2e656a
dchambers <dominic.chambers@gmail.com>
2015-11-02 13:33:43 +0000

    Clean up model on unsubscribe
    Use `state.unset()` to clean-up, rather than nulling down with
    `state.set()`.

--- src/actions/unsubscribe.js
+++ src/actions/unsubscribe.js
@@ -4,7 +4,7 @@ export default function unsubscribe(input, state, output, services) {
 	const subscription = getSubscription(input.topic);
 	subscription.removeSubscriber();
 
-	state.set(input.topic + '.subscribers.' + input.instance, false);
-	state.set(input.topic + '.bid', null);
-	state.set(input.topic + '.ask', null);
+	state.unset(input.topic + '.subscribers.' + input.instance);
+	state.unset(input.topic + '.bid');
+	state.unset(input.topic + '.ask');
 }
